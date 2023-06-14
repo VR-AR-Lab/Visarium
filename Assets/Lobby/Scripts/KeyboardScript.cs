7a6c4c1d660fe844e3a81295ef6f29c7f0c9a1f3
Sergey Antonov <59932798+Sergeo-Sss@users.noreply.github.com>
2023-06-14 11:48:14 +0300

    Create VR Lobby

---
+++ Assets/Lobby/Scripts/KeyboardScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class KeyboardScript : MonoBehaviour
+{
+
+    [Header("Click sound")]
+    public AudioClip click;
+    public AudioSource player;
+
+    private TMP_InputField TextField;
+
+    [Header("Keyboards")]
+    public GameObject RusLayoutSml, RusLayoutBig, EngLayoutSml, EngLayoutBig, SymbLayout;
+
+    public void alphabetFunction(string alphabet)
+    {
+        player.PlayOneShot(click);
+        TextField.text=TextField.text + alphabet;
+    }
+
+    public void BackSpace()
+    {
+        player.PlayOneShot(click);
+        if (TextField.text.Length>0) TextField.text= TextField.text.Remove(TextField.text.Length-1);
+    }
+
+    public void Enter()
+    {
+        TextField.DeactivateInputField();
+        gameObject.SetActive(false);
+    }
+
+    public void CloseAllLayouts()
+    {
+        player.PlayOneShot(click);
+        RusLayoutSml.SetActive(false);
+        RusLayoutBig.SetActive(false);
+        EngLayoutSml.SetActive(false);
+        EngLayoutBig.SetActive(false);
+        SymbLayout.SetActive(false);
+    }
+
+    public void ShowLayout(GameObject SetLayout)
+    {
+        player.PlayOneShot(click);
+        CloseAllLayouts();
+        SetLayout.SetActive(true);
+    }
+    public void OpenKeyboard()
+    {
+        TextField = EventSystem.current.currentSelectedGameObject.GetComponent<TMP_InputField>();
+        gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Lobby/Scripts/LobbyManager.cs
@@ -0,0 +1,195 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using System.IO;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    [Header("Groups W-C")]
+    public GameObject[] Group;
+
+    [Header("Sounds")]
+    public AudioSource player;
+    public AudioClip fonsound;
+    public AudioClip click;
+
+    [Header("Network room")]
+    public ScrollRect scrollRect;
+    public GameObject scrollContent;
+    public GameObject connectPrefab;
+
+    [Header("Room options")]
+    public TMP_InputField roomname;
+    public Toggle Visible;
+    public Toggle Public;
+    public TMP_InputField maxplayers;
+    public TMP_Text errortxt;
+
+    [Header("Login component")]
+    public GameObject login;
+    public GameObject registr;
+    public GameObject confirm;
+    public GameObject reset;
+
+    public void GoGroup(int ID)
+    {
+        player.PlayOneShot(click);
+        for (int i = 0; i < Group.Length; i++)
+        {
+            if (i == ID)
+            {
+                Group[i].SetActive(true);
+                continue;
+            }
+            Group[i].SetActive(false);
+        }
+    }
+
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonStartUp();
+    }
+
+    private void Start()
+    {
+        scrollRect.verticalNormalizedPosition = 1;
+        errortxt.text = "";
+    }
+
+    public void SetGroup(string roomname, int countPlayers, int maxPlayers)
+    {
+        GameObject scrolltemobj = Instantiate(connectPrefab);
+        scrolltemobj.name =  roomname;
+        scrolltemobj.transform.Find("room name").GetComponent<TMP_Text>().text = roomname;
+        scrolltemobj.transform.Find("players").GetComponent<TMP_Text>().text = countPlayers + "/" + maxPlayers + " players";
+        scrolltemobj.transform.Find("connect").GetComponent<Button>().onClick.AddListener(delegate () { ConnectToGameRoom(roomname); });
+        scrolltemobj.transform.SetParent(scrollContent.transform, false);
+    }
+
+    public void ChangeLoginGroup()
+    {
+        player.PlayOneShot(click);
+        if (login.activeSelf)
+        {
+            login.SetActive(false);
+            registr.SetActive(true);
+        }
+        else if(registr.activeSelf)
+        {
+            login.SetActive(true);
+            registr.SetActive(false);
+        }
+        else if(confirm.activeSelf)
+        {
+            login.SetActive(true);
+            confirm.SetActive(false);
+            registr.SetActive(false);
+        }
+        else if (reset.activeSelf)
+        {
+            reset.SetActive(false);
+            login.SetActive(true);
+            confirm.SetActive(false);
+            registr.SetActive(false);
+        }
+    }
+    public void ConfirmEmailGroup()
+    {
+        registr.SetActive(false);
+        confirm.SetActive(true);
+        reset.SetActive(false);
+    }
+
+    public void PlaySoundCLick()
+    {
+        player.PlayOneShot(click);
+    }
+    //public void Logout()
+    //{
+    //    GetComponent<AuthManager>().FirebaseLogOut();
+    //    GoGroup(2);
+    //}
+    public void ActiveResetGroup()
+    {
+        player.PlayOneShot(click);
+        login.SetActive(false);
+        reset.SetActive(true);
+    }
+
+    // Photon Start
+
+    public void PhotonStartUp()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+        base.OnConnectedToMaster();
+    }
+
+    public void CreateRoom()
+    {
+        player.PlayOneShot(click);
+        int max;
+        bool isInt = Int32.TryParse(maxplayers.text, out max);
+        if(max<=0)
+        {
+            errortxt.text = "ERROR: the number of players is incorrect";
+        }
+        else if (roomname.text.Length==0)
+        {
+            errortxt.text = "ERROR: come up with the name of the room";
+        }
+        else
+        {
+            //PhotonNetwork.NickName = GetComponent<AuthManager>().User.DisplayName;
+            PhotonNetwork.NickName = $"Player {UnityEngine.Random.Range(1, 100)}";
+            RoomOptions roomOptions = new RoomOptions() { IsVisible = Visible, IsOpen = Public, MaxPlayers = (byte)max };
+            PhotonNetwork.JoinOrCreateRoom(roomname.text, roomOptions, TypedLobby.Default);
+        }
+    }
+
+    private void ConnectToGameRoom(string roomname)
+    {
+        //PhotonNetwork.NickName = GetComponent<AuthManager>().User.DisplayName;
+        PhotonNetwork.NickName = $"Player {UnityEngine.Random.Range(1, 100)}";
+        PhotonNetwork.JoinRoom(roomname);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+        base.OnJoinedRoom();
+    }
+
+    private void OnDestroyRoomPrefab(string roomname)
+    {
+        Destroy(GameObject.Find(roomname));
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (RoomInfo info in roomList)
+        {
+            if (info.RemovedFromList)
+            {
+                print("removed "+info.Name);
+                OnDestroyRoomPrefab(info.Name);
+            }
+            else
+            {
+                SetGroup(info.Name, info.PlayerCount, info.MaxPlayers);
+            }
+        }
+        base.OnRoomListUpdate(roomList);
+    }
+}

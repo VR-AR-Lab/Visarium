78c8cc3d6dbc61e13b4e042b02e7511b4d004611
sergeychaliy2 <70662544+sergeychaliy2@users.noreply.github.com>
2023-07-11 13:10:22 +0300

    .
    .

--- Assets/Scripts/PlaneRaycast.cs
+++ Assets/Scripts/PlaneRaycast.cs
@@ -1,35 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlaneRaycast : MonoBehaviour
 {
 
     public Transform Pointer;
     bool Pressed = false;
+    public float distantion;
     void Update()
     {
         Camera cameraAr = Camera.main;
         Ray ray = new Ray(transform.position, transform.forward);
-        Debug.DrawRay(transform.position, transform.forward * 1, Color.red);
+        Debug.DrawRay(transform.position, transform.forward * 2, Color.red);
         RaycastHit hit;
-        if (Physics.Raycast(ray,out hit))
+        if (Physics.Raycast(ray, out hit))
         {
             Pointer.position = hit.point;
-        }
-        if (Pressed)
-        {
-            hit.transform.position = cameraAr.transform.position + cameraAr.transform.forward + cameraAr.transform.forward + cameraAr.transform.forward;
-            hit.rigidbody.isKinematic = true;
-            Debug.Log("Take target");
+            distantion = Vector3.Distance(Pointer.position, transform.position);
+            if (distantion < 4.0)
+            {
+                if (Pressed)
+                {
+                    if (hit.collider.tag == "Draggable")
+                        hit.transform.position = cameraAr.transform.position + cameraAr.transform.forward *3;
+                    hit.rigidbody.isKinematic = true;
 
-        }
-        else if (Physics.Raycast(ray, out hit) && (Pressed))
-        {
-            hit.rigidbody.isKinematic = true;
-        }else if (!Pressed)
-        {
-            hit.rigidbody.isKinematic = false;
+                    Debug.Log("Take target");
+                }
+                else if (!Pressed)
+                {
+                    Pressed = false;
+                    hit.rigidbody.isKinematic = false;
+                }
+            }
         }
     }
     public void onDown()
